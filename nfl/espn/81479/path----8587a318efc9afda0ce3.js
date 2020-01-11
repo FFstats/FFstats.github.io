c08090a86cda08cf189b2205b2bd89d2e647db90
Fabian Brenes <fabian@squidsource.com>
2020-01-10 19:47:19 -0600

    nfl/espn/81479

---
+++ nfl/espn/81479/path----8587a318efc9afda0ce3.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{202:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"The Schwab",id:"81479",isPrivate:!0}]}}]}},layoutContext:{}}}});
+//# sourceMappingURL=path----8587a318efc9afda0ce3.js.map
