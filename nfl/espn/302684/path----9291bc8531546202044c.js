07ba7a2dabf9f2a956c88c0a3c7386aa854cff8e
Fabian Brenes <fabian@squidsource.com>
2020-01-10 19:25:42 -0600

    nfl/espn/302684

---
+++ nfl/espn/302684/path----9291bc8531546202044c.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{202:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"Going For 2",id:"302684",isPrivate:!0}]}}]}},layoutContext:{}}}});
+//# sourceMappingURL=path----9291bc8531546202044c.js.map
