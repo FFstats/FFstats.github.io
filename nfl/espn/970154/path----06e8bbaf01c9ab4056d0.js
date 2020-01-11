c7e085b5043bf44421ecc3115ea866a425125a81
Fabian Brenes <fabian@squidsource.com>
2020-01-10 19:31:03 -0600

    nfl/espn/970154

---
+++ nfl/espn/970154/path----06e8bbaf01c9ab4056d0.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{202:function(a,e){a.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"Password Is Taco",id:"970154",isPrivate:!0}]}}]}},layoutContext:{}}}});
+//# sourceMappingURL=path----06e8bbaf01c9ab4056d0.js.map
