44cc69a31b97e6857a5eee5532e683234161b9b7
Fabian Brenes <fabian@squidsource.com>
2020-01-14 13:22:56 -0600

    nfl/espn/8841

--- nfl/espn/8841/path----ff64724e839c78191e95.js
+++ nfl/espn/8841/path----ff64724e839c78191e95.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{202:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"FFL",id:"8841",isPrivate:!0}]}}]}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{210:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"FFL",id:"8841",isPrivate:!0}]}}]}},layoutContext:{}}}});
 //# sourceMappingURL=path----ff64724e839c78191e95.js.map
