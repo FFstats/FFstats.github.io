e71cba96b7dc221c0ecf9d06697367260627237a
Fabian Brenes <fabian@squidsource.com>
2020-10-20 11:39:54 -0600

    nfl/espn/902198

--- nfl/espn/902198/path----91f2786a94bba425fd97.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{210:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"The Zeeb League",id:"902198",isPrivate:!1}]}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----91f2786a94bba425fd97.js.map
