92ef85892e627e190e3894086ab0a458b16a956b
Fabian Brenes <fabian@squidsource.com>
2021-10-19 22:18:17 -0600

    nfl/espn/408978 - 2021 Update

--- nfl/espn/408978/path----0b7a09895717a3c748b6.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{210:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"NJ$treakers International",id:"408978",isPrivate:!1}]}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----0b7a09895717a3c748b6.js.map
