37e7e3e50ae5c3537dac30dbb9f4336714db6510
Fabian Brenes <fabian@squidsource.com>
2021-10-19 22:43:45 -0600

    nfl/espn/56226 - 2021 Update

--- nfl/espn/56226/path----faf87098df663a32a7fa.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{210:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"Hit or Miss",id:"56226",isPrivate:!0}]}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----faf87098df663a32a7fa.js.map
