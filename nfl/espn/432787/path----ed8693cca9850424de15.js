2bb3cb507c8bf52ecc803fdeabe8120479b63577
Fabian Brenes <fabian@squidsource.com>
2021-10-19 22:23:19 -0600

    nfl/espn/432787 - 2021 Update

--- nfl/espn/432787/path----ed8693cca9850424de15.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{210:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"Down To Football",id:"432787",isPrivate:!1}]}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----ed8693cca9850424de15.js.map
