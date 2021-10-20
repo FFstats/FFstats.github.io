960336aa8a15456ba15c41c620c2423fbd2faa23
Fabian Brenes <fabian@squidsource.com>
2021-10-19 20:24:32 -0600

    nfl/espn/1880649 - 2021 Update

--- nfl/espn/1880649/path----9a8c884bf9f76b4607ce.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{202:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"League of Shame",id:"1880649",isPrivate:!0}]}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----9a8c884bf9f76b4607ce.js.map
