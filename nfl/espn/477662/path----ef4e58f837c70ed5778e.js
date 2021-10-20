ad242a6128bb31f9b57cc980306e2928a4c06d7c
Fabian Brenes <fabian@squidsource.com>
2021-10-19 22:30:00 -0600

    nfl/espn/477662 - 2021 Update

--- nfl/espn/477662/path----ef4e58f837c70ed5778e.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{210:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"Dinner and Beer League",id:"477662",isPrivate:!1}]}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----ef4e58f837c70ed5778e.js.map
