1d8379643e73b3242e26acb78ae5cdc15dc12509
Fabian Brenes <fabian@squidsource.com>
2022-07-28 16:50:54 -0600

    nfl/espn/8841 - 2021 Update

--- nfl/espn/8841/path----ff64724e839c78191e95.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{210:function(e,a){e.exports={data:{allDataJson:{edges:[{node:{leagueSettings:[{leagueName:"FFL",id:"8841",isPrivate:!0}]}}]}},layoutContext:{}}}});
-//# sourceMappingURL=path----ff64724e839c78191e95.js.map
